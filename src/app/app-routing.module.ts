53fdfbcd55be6684f377d48262efda74fd62bd87
Javier Paz <luis.j.paz@unl.edu.ec>
2021-02-03 14:17:34 -0500

    Se ha implementado la creacion de la pantalla de visualizacion de videos, Se configuro para quie se lance por defecto esta pantalla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+/*---------------------*/
+import {HeadersComponent} from './components/home/headers/headers.component';
+import {HomeVideoComponent} from './components/home-video/home-video.component';
+/*----------------------*/
+const routes: Routes = [
 
-const routes: Routes = [];
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home/views'
+  },
+  {
+    path: 'home',
+    component: HeadersComponent,
+  },
+  {
+    path: 'home/views',
+    component: HomeVideoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeadersComponent } from './components/home/headers/headers.component';
+import { HomeVideoComponent } from './components/home-video/home-video.component';
+
+import { ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadersComponent
+    HeadersComponent,
+    HomeVideoComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
