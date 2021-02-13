3826af6b7c74e4e396cf69bd5fc2de7dd368741b
Luis Javier Paz Cañar <luis.j.paz@unl.edu.ec>
2021-02-13 02:33:20 -0500

    Se ha implementado la estructura de la plantilla de ver videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
+import {HeadersComponent} from './components/home/headers/headers.component';
+import {HomeVideoComponent} from './components/home-video/home-video.component';
+import {RestServicesRestVideoService} from './services/rest-services-rest-video.service';
+import { NgxDropzoneModule} from 'ngx-dropzone';
 import { HeadersComponent } from './components/home/headers/headers.component';
 import { HomeVideoComponent } from './components/home-video/home-video.component';
 import { RestServicesRestVideoService} from './services/rest-services-rest-video.service';
@@ -53,7 +57,6 @@ import { ViewVideoComponent } from './components/view-video/view-video.component
     SectionHomeVideoComponent,
     SectionSearchVideoComponent,
     ViewVideoComponent
-    HomeVideoComponent
 
     HeadersComponent,
     PrincipalComponent,
@@ -82,13 +85,17 @@ import { ViewVideoComponent } from './components/view-video/view-video.component
     MatSidenavModule,
     NgxDropzoneModule,
     ReactiveFormsModule,
+    NgxDropzoneModule,
     HttpClientModule,
     CloudinaryModule.forRoot(Cloudinary, {
       cloud_name: 'dzkcr9eec', upload_preset: 'lp8lafrr'
+    }),
+
     })
     NgxSpinnerModule,
   ],
-  providers: [RestServicesRestVideoService, SearchVideoServiceService, SectionVideoVisibleServiceService ],
+  providers: [RestServicesRestVideoService, SearchVideoServiceService, SectionVideoVisibleServiceService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/home-video/nav-navigation/nav-navigation.component.ts
+++ src/app/components/home-video/nav-navigation/nav-navigation.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {SectionVideoVisibleServiceService} from '../../../services/servicesVideo/visibeSections/section-video-visible-service.service';
-
+import { NgbModal} from '@ng-'
 
 @Component({
   selector: 'app-nav-navigation',
@@ -10,11 +10,34 @@ import {SectionVideoVisibleServiceService} from '../../../services/servicesVideo
 })
 export class NavNavigationComponent implements OnInit {
   nameSearchVideo = new FormControl('');
-  constructor(public visibleSectionEvent: SectionVideoVisibleServiceService) { }
+  modalVisible = true;
+  files: File[] = [];
+  constructor(public visibleSectionEvent: SectionVideoVisibleServiceService, private modalUpload: NgbModal) {
+  }
 
   ngOnInit(): void {
   }
-  methodSearchVideo(): void{
+
+  methodSearchVideo(): void {
     this.visibleSectionEvent.setVisibleSectionHome(false);
+  }
+
+  modelUpload(): any {
+  }
+
+  modelDetalle(): any {
+  }
+  upload(upload: any): any{
+
+  }
+
+  onSelect(event: any): void {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+
+  onRemove(event: any): void {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
   }
 }
