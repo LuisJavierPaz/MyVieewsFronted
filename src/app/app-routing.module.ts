8675b164870aadab235fb75ec2f0c274afd8a7c4
Luis Javier Paz Cañar <luis.j.paz@unl.edu.ec>
2021-02-13 01:04:27 -0500

    Se ha implementado la estructura de la plantilla de ver videos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,15 @@ import { PrincipalComponent } from './components/perfil/principal/principal.comp
 
 
 
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+/*---------------------*/
+import {HeadersComponent} from './components/home/headers/headers.component';
+import {HomeVideoComponent} from './components/home-video/home-video.component';
+import { ViewVideoComponent} from './components/view-video/view-video.component';
+/*----------------------*/
 const routes: Routes = [
+
   {
     path: '',
     pathMatch: 'full',
@@ -32,12 +40,12 @@ const routes: Routes = [
     component: PrincipalComponent
   },
   {
-    path: 'home',
-    component: HeadersComponent,
-  },
-  {
     path: 'home/views',
     component: HomeVideoComponent
+  },
+  {
+    path: 'home/play',
+    component: ViewVideoComponent
   }
 ];
 
@@ -45,4 +53,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,14 +31,28 @@ import { NavNavigationComponent } from './components/home-video/nav-navigation/n
 import { AsideMenuComponent } from './components/home-video/aside-menu/aside-menu.component';
 import { SectionHomeVideoComponent } from './components/home-video/section-home-video/section-home-video.component';
 import { SectionSearchVideoComponent } from './components/home-video/section-search-video/section-search-video.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NavNavigationComponent} from './components/home-video/nav-navigation/nav-navigation.component';
+import {AsideMenuComponent} from './components/home-video/aside-menu/aside-menu.component';
+import {SectionHomeVideoComponent} from './components/home-video/section-home-video/section-home-video.component';
+import {SectionSearchVideoComponent} from './components/home-video/section-search-video/section-search-video.component';
 import {SearchVideoServiceService} from './services/servicesVideo/search-video-service.service';
 import {SectionVideoVisibleServiceService} from './services/servicesVideo/visibeSections/section-video-visible-service.service';
 import { LoginComponent } from './components/login/login.component';
+import {CloudinaryModule} from '@cloudinary/angular-5.x';
+import * as Cloudinary from 'cloudinary-core';
+import { ViewVideoComponent } from './components/view-video/view-video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadersComponent,
+    HomeVideoComponent,
+    NavNavigationComponent,
+    AsideMenuComponent,
+    SectionHomeVideoComponent,
+    SectionSearchVideoComponent,
+    ViewVideoComponent
     HomeVideoComponent
 
     HeadersComponent,
@@ -69,6 +83,9 @@ import { LoginComponent } from './components/login/login.component';
     NgxDropzoneModule,
     ReactiveFormsModule,
     HttpClientModule,
+    CloudinaryModule.forRoot(Cloudinary, {
+      cloud_name: 'dzkcr9eec', upload_preset: 'lp8lafrr'
+    })
     NgxSpinnerModule,
   ],
   providers: [RestServicesRestVideoService, SearchVideoServiceService, SectionVideoVisibleServiceService ],
